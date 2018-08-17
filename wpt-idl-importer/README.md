a5cd9078cc88d016afdd56c806615f5bc1c56889
Philip Jägenstedt <philip@foolip.org>
2018-08-17 14:29:28 +0200

    Copy script from https://github.com/foolip/wpt-idl-importer

---
+++ wpt-idl-importer/README.md
@@ -0,0 +1,6 @@
+This script copies `*.idl` files from [whatwg/idl/](/whatwg/idl/) in this repo
+into [interfaces/](https://github.com/web-platform-tests/wpt/tree/master/interfaces) in wpt.
+
+The scripts need `GH_TOKEN` environment variable set to a
+[personal access token](https://github.com/settings/tokens/new) with the
+"Access public repositories" (public_repo) scope enabled.
---
+++ wpt-idl-importer/update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+reffy_sha=`git rev-parse --short HEAD`
+
+git clone --single-branch https://github.com/web-platform-tests/wpt.git
+
+cp whatwg/idl/*.idl wpt/interfaces/
+
+cd wpt
+
+git remote add fork https://autofoolip:$GH_TOKEN@github.com/autofoolip/wpt.git
+git push fork master
+
+# First handle added (untracked, --other) and modified files.
+# A temp file is needed because `git ls-files` holds index.lock.
+tmpfile=`mktemp`
+git ls-files --modified --others --exclude-standard > "$tmpfile"
+cat "$tmpfile" | while read path; do
+    echo "Handling $path"
+    shortname=`basename $path .idl`
+    branchname="reffy-reports/$shortname"
+    git checkout -b $branchname origin/master
+    git add "$path"
+    git commit -F - << EOM
+Update $path
+
+Source: https://github.com/tidoust/reffy-reports/blob/$reffy_sha/whatwg/idl/$shortname.idl
+Build: https://travis-ci.org/tidoust/reffy-reports/builds/$TRAVIS_BUILD_ID
+EOM
+    git push -f fork $branchname
+done
+rm "$tmpfile"
+
+# TODO: Check if wpt has IDL files not in reffy-reports and list them.
