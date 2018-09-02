f8ba65d002db82c2f34dc1e3bdd846a595abfb41
Philip Jägenstedt <philip@foolip.org>
2018-09-03 00:13:28 +0200

    echo

--- wpt-idl-importer/update.sh
+++ wpt-idl-importer/update.sh
@@ -14,6 +14,7 @@ cd wpt
 
 echo "Status after syncing IDL files:"
 git status --short
+echo
 
 git remote add fork https://autofoolip:$GH_TOKEN@github.com/autofoolip/wpt.git
 git push -q fork master
