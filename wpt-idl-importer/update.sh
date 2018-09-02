c05d5e819bed64ce212789c16636ce18c9e261cc
Philip Jägenstedt <philip@foolip.org>
2018-09-02 23:44:56 +0200

    try again

--- wpt-idl-importer/update.sh
+++ wpt-idl-importer/update.sh
@@ -16,13 +16,21 @@ git remote add fork https://autofoolip:$GH_TOKEN@github.com/autofoolip/wpt.git
 git push fork master
 
 # Handle added (untracked, --other), removed and modified files.
-# A temp file is needed because `git ls-files` holds index.lock.
-tmpfile=`mktemp`
-git ls-files --others --exclude-standard | sed 's/^/Add /' > "$tmpfile"
-git ls-files --deleted | sed 's/^/Remove /' >> "$tmpfile"
-git ls-files --modified | sed 's/^/Update /' >> "$tmpfile"
-cat "$tmpfile" | while read action path; do
+# A temp file is needed because `git status` holds index.lock.
+statusfile=`mktemp`
+git status --porcelain > "$statusfile"
+cat "$statusfile" | while read status path; do
     echo "Handling $path"
+    case "$status" in
+        M)
+            action="Update"
+            ;;
+        D)
+            action="Remove"
+            ;;
+        *)
+            action="Add"
+    esac
     shortname=`basename $path .idl`
     branchname="reffy-reports/$shortname"
     git checkout -b $branchname origin/master
@@ -35,6 +43,6 @@ Build: https://travis-ci.org/tidoust/reffy-reports/builds/$TRAVIS_BUILD_ID
 EOM
     git push -f fork $branchname
 done
-rm "$tmpfile"
+rm "$statusfile"
 
 # TODO: Check if wpt has IDL files not in reffy-reports and list them.
