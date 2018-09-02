d2545ae058825a8b045bdb947835d09b51aa9c60
Philip Jägenstedt <philip@foolip.org>
2018-09-03 00:02:26 +0200

    tinker more

--- wpt-idl-importer/update.sh
+++ wpt-idl-importer/update.sh
@@ -11,16 +11,19 @@ rm wpt/interfaces/*.idl
 cp whatwg/idl/*.idl wpt/interfaces/
 
 cd wpt
-echo "Status:"
+
+echo "Status after syncing IDL files:"
 git status --short
 
 git remote add fork https://autofoolip:$GH_TOKEN@github.com/autofoolip/wpt.git
 git push -q fork master
 
-# Handle added (untracked, --other), removed and modified files.
+# Use `git status` to list added, removed and modified files.
 # A temp file is needed because `git status` holds index.lock.
 statusfile=`mktemp`
 git status --porcelain > "$statusfile"
+# Each branch pushed is appended to this file, so that PRs can be created.
+branchfile=`mktmp`
 cat "$statusfile" | while read status path; do
     echo "Handling $path"
     case "$status" in
@@ -30,22 +33,25 @@ cat "$statusfile" | while read status path; do
         D)
             action="Remove"
             ;;
-        *)
+        *) # "??" for untracked and anything else
             action="Add"
     esac
     shortname=`basename $path .idl`
     branchname="reffy-reports/$shortname"
     git checkout -q -b "$branchname" origin/master
     git add "$path"
-    git commit -F - << EOM
+    git commit -q -F - << EOM
 $action $path
 
 Source: https://github.com/tidoust/reffy-reports/blob/$reffy_sha/whatwg/idl/$shortname.idl
 Build: https://travis-ci.org/tidoust/reffy-reports/builds/$TRAVIS_BUILD_ID
 EOM
     git push -q -f fork "$branchname"
+    echo "$branchname" >> "$branchfile"
     # show what was pushed
-    git log --decorate --no-walk
+    git log --no-walk
     echo
 done
-rm "$statusfile"
+
+echo "Branches updated:"
+cat "$branchfile"
