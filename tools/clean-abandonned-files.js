fe00d22ef604bb826ab902f1d1388602da428b25
Dominique Hazael-Massieux <dom@w3.org>
2020-11-12 15:18:59 +0100

    Compare file list to correct index

--- tools/clean-abandonned-files.js
+++ tools/clean-abandonned-files.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const ed = require("../ed/index.json");
+const tr = require("../tr/index.json");
 
 const subdirs = ["dfns", "css", "headings", "idl", "idlparsed", "links", "refs"];
 
@@ -10,17 +11,17 @@ const removeExtension = f => {
   return components.slice(0, components.length - 1).join(".");
 }
 
-function checkDir(path) {
+function checkDir(path, index) {
   const dir = fs.readdirSync(path);
   for (let filename of dir) {
     const subdir = path.split("/")[1];
-    if (!ed.results.find(spec => spec[subdir] === subdir + "/" + filename)) {
+    if (!index.results.find(spec => spec[subdir] === subdir + "/" + filename)) {
       fs.unlinkSync(path + "/" + filename);
     }
   }
 }
 
 for (let dir of subdirs) {
-  checkDir("ed/" + dir);
-  checkDir("tr/" + dir);
+  checkDir("ed/" + dir, ed);
+  checkDir("tr/" + dir, tr);
 }
