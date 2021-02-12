d275cc5db34ae07b293d377a2f238490f12931c5
Philip Jägenstedt <philip@foolip.org>
2021-02-12 09:00:19 +0100

    Replace hardcoded 198 test

--- test/idl/all.js
+++ test/idl/all.js
@@ -5,7 +5,7 @@ const idl = require('@webref/idl');
 describe('@webidl/idl module', () => {
   it('listAll', async () => {
     const files = await idl.listAll();
-    assert.strictEqual(Object.keys(files).length, 198);
+    assert(Object.keys(files).length > 0);
     for (const [shortname, file] of Object.entries(files)) {
       assert.strictEqual(shortname, file.shortname);
       assert(/^[a-z0-9-]+$/i.exec(shortname),
@@ -15,7 +15,7 @@ describe('@webidl/idl module', () => {
 
   it('parseAll', async () => {
     const all = await idl.parseAll();
-    assert.strictEqual(Object.keys(all).length, 198);
+    assert(Object.keys(all).length > 0);
     for (const ast of Object.values(all)) {
       assert(Array.isArray(ast));
       assert(ast.length);
