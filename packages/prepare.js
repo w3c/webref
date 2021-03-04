a006a939b7ad09c9461a54e1031e568af9b4a466
Francois Daoust <fd@tidoust.net>
2021-03-04 16:12:47 +0100

    Add first series of CSS patches
    The patches cover obvious grammar issues.

--- packages/prepare.js
+++ packages/prepare.js
@@ -7,21 +7,24 @@ const rootDir = path.resolve(__dirname, '..');
 
 const packages = [
   {
+    name: 'idl',
     srcDir: path.join(rootDir, 'ed/idl'),
     dstDir: path.join(rootDir, 'packages/idl'),
     patchDir: path.join(rootDir, 'ed/idlpatches'),
     fileExt: 'idl'
   },
   {
+    name: 'css',
     srcDir: path.join(rootDir, 'ed/css'),
     dstDir: path.join(rootDir, 'packages/css'),
+    patchDir: path.join(rootDir, 'ed/csspatches'),
     fileExt: 'json'
   },
 ];
 
 
 async function main() {
-  for (const { srcDir, dstDir, patchDir, fileExt } of packages) {
+  for (const { name, srcDir, dstDir, patchDir, fileExt } of packages) {
     // rm dstDir/*.${fileExt}
     const dstFiles = await fs.readdir(dstDir);
     for (const file of dstFiles) {
@@ -46,7 +49,7 @@ async function main() {
         if (file.endsWith('.patch')) {
           const patch = path.join(patchDir, file);
           console.log(`Applying ${file}`);
-          await execFile('git', ['apply', '--directory=packages/idl', '-p3', patch], {
+          await execFile('git', ['apply', `--directory=packages/${name}`, '-p3', patch], {
             cwd: rootDir
           });
         }
