7a211e4156ba6b0ba888253d5b56cb665f81b5d1
Francois Daoust <fd@tidoust.net>
2021-03-10 11:42:30 +0100

    Make sure keys exist, use doesNotThrow

--- test/css/validate.js
+++ test/css/validate.js
@@ -14,16 +14,15 @@ css.listAll().then(all => {
   for (const [shortname, data] of Object.entries(all)) {
     describe(`The ${shortname} entry in @webidl/css`, () => {
       for (const { type, prop, value } of cssValues) {
-        if (!data[prop]) {
-          continue;
-        }
         for (const [name, desc] of Object.entries(data[prop])) {
           if (!desc[value]) {
             continue;
           }
           it(`defines a valid ${type} "${name}"`, () => {
-            const ast = definitionSyntax.parse(desc[value]);
-            assert(ast.type);
+            assert.doesNotThrow(() => {
+              const ast = definitionSyntax.parse(desc[value]);
+              assert(ast.type);
+            }, `Invalid definition value: ${desc[value]}`);
           });
         }
       }
