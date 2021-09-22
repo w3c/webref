0efb3540eabcf6b7c41adae4ff2d946be25e4136
François Daoust <fd@w3.org>
2021-09-22 17:59:12 +0200

    Update test/css/package.js
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>

--- test/css/package.js
+++ test/css/package.js
@@ -3,7 +3,7 @@ const assert = require('assert').strict;
 const cssPackage = require('../../packages/css/package.json');
 const rootPackage = require('../../package.json');
 
-describe('The @webidl/css package', () => {
+describe('The @webref/css package', () => {
   it('uses the same version of css-tree as main package', () => {
     assert.equal(cssPackage.peerDependencies['css-tree'],
                  `^${rootPackage.devDependencies['css-tree']}`);
