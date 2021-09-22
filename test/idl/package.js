927b130502faace00cb769371afb0cd5fbb1f3c4
François Daoust <fd@w3.org>
2021-09-22 17:59:02 +0200

    Update test/idl/package.js
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>

--- test/idl/package.js
+++ test/idl/package.js
@@ -3,7 +3,7 @@ const assert = require('assert').strict;
 const idlPackage = require('../../packages/idl/package.json');
 const rootPackage = require('../../package.json');
 
-describe('The @webidl/idl package', () => {
+describe('The @webref/idl package', () => {
   it('uses the same version of webidl2.js as main package', () => {
     assert.equal(idlPackage.peerDependencies.webidl2,
                  `^${rootPackage.devDependencies.webidl2}`);
