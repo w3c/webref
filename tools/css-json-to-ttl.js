b97fa30d2efc8331c341e40f60f909f8c04d9f0f
Francois Daoust <fd@tidoust.net>
2021-09-23 11:07:41 +0200

    Add editorial improvements to CSS ontology generation
    Implements suggestions in https://github.com/w3c/webref/pull/327

--- tools/css-json-to-ttl.js
+++ tools/css-json-to-ttl.js
@@ -17,13 +17,13 @@ console.log(`@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.
 @prefix css: <http://www.w3.org/ns/css#> .
 
 <> rdfs:comment """This ontology allows CSS properties to be expressed in RDF.
-The same camelcase convention is usede as is used by the subproperties of the
+The same camelcase convention is used as is used by the subproperties of the
  style property in browser JS implementations.
 
  """ .
   `)
 for (const prop in cssData.properties) {
-  const camel = camelCasify(prop)
+  const camel = cssData.properties[prop].styleDeclaration.pop()
   // console.log(`  Property ${prop} - ${camel}`)
   console.log(`css:${camel} a rdf:Property; rdfs:label "${prop}"; spec:values`)
 }
