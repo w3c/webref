ea745cb25d094934d5a877f6030f348a1a593f72
François Daoust <fd@w3.org>
2022-07-22 09:14:17 +0200

    Events package "listAll": don't preserve list in memory (#680)
    The `listAll` function should not assume that callers are willing to preserve
    the list of events in memory. Besides, if callers modify the list in place, they
    would expect another call to `listAll` to return a fresh list.

--- packages/events/index.js
+++ packages/events/index.js
@@ -1,13 +1,9 @@
 const fs = require('fs').promises;
 const path = require('path');
-let events;
 
 async function listAll({folder = __dirname} = {}) {
-  if (!events) {
-    const json = await fs.readFile(path.join(folder, 'events.json'), 'utf8');
-    events = JSON.parse(json);
-  }
-  return events;
+  const json = await fs.readFile(path.join(folder, 'events.json'), 'utf8');
+  return JSON.parse(json);
 }
 
 module.exports = {listAll};
