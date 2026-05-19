aebcbd71e6c72425d8595bfc0258b643b3c62e08
Francois Daoust <fd@tidoust.net>
2026-05-19 08:54:29 +0200

    Publish npm package, add tag and update latest branch

--- tools/release-autopackages.js
+++ tools/release-autopackages.js
@@ -52,38 +52,67 @@ async function releaseXrefPackage() {
     }
   }
 
-  // TODO: Increase the version number in the package.json file
-  // TODO: Commit/Push the change (and create a tag?)
-  // TODO: Publish the npm package.
+  console.log();
+  console.log('Bump the package version');
+  const packageFile = path.join(packageFolder, 'package.json');
+  const packageContents = await loadJSON(packageFile);
+  const version = packageContents.version;
+  console.log(`- current version: ${version}`);
+  const reVersion = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)?$/;
+  const versionTokens = version.match(reVersion);
+  const major = parseInt(versionTokens[1], 10);
+  const minor = parseInt(versionTokens[2], 10);
+  const patch = parseInt(versionTokens[3], 10);
+  const newVersion = `${major}.${minor}.${patch+1}`;
+  console.log(`- new version: ${version}`);
+  packageContents.version = newVersion;
+  await fs.writeFile(packageFile, JSON.stringify(packageContents, null, 2), 'utf8');
+
+  console.log();
+  console.log('Commit new version');
+  execSync(`git add ${packageFile}`);
+  execSync(`git commit -m "Bump @webref/xref version from ${version} to ${newVersion}"`);
+  execSync('git push origin HEAD:main');
+  console.log('- done');
+  console.log();
+
+  console.log();
+  console.log('Publish new version to npm');
+  console.log(`- publish packages/${type} folder to npm`);
+  const pubOptions = {
+    package: packageFile
+    //, debug: console.debug
+  };
+  if (NPM_TOKEN) {
+    pubOptions.token = NPM_TOKEN;
+  }
+  const pubResult = await npmPublish(pubOptions);
+  console.log(`- published version was ${pubResult.oldVersion}`);
+  console.log(`- published version is ${pubResult.version}`);
+
+  console.log();
+  console.log('Tag released commit');
+  const prefix = '@webref/xref@';
+  const tag = prefix + pubResult.version;
+  execSync('git tag ${tag}');
+  execSync('git push origin ${tag}');
+
+  console.log();
+  console.log('Update @webref/xref@latest branch');
+  const latest = prefix + 'latest';
+  execSync(`git push origin main:${latest}`);
 }
 
 
 /*******************************************************************************
-Retrieve tokens from environment, prepare Octokit and kick things off
+Retrieve tokens from environment and kick things off
 *******************************************************************************/
-const config = await loadJSON("config.json");
-const GH_TOKEN = config?.GH_TOKEN ?? process.env.GH_TOKEN;
-if (!GH_TOKEN) {
-  console.error("GH_TOKEN must be set to some personal access token as an env variable or in a config.json file");
-  process.exit(1);
-}
-
 // An NPM token is needed to run the script from a local machine.
 // Authentication from a GitHub workflow rather relies on OpenID Connect
 // and the release workflow must be added as a trusted publisher for each
 // npm package that can be released, see:
 // https://docs.npmjs.com/trusted-publishers
 const NPM_TOKEN = config?.NPM_TOKEN ?? process.env.NPM_TOKEN;
-
-// Note: npm-publish has a bug and needs an "INPUT_TOKEN" env variable:
-// https://github.com/JS-DevTools/npm-publish/issues/15
-// (we're passing the token to the function directly, no need to set it here)
-process.env.INPUT_TOKEN = "";
-
-const octokit = new Octokit({
-  auth: GH_TOKEN
-  //, log: console
-});
 
 releaseXrefPackage()
   .then(() => {
