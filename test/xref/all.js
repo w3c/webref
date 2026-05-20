972a74d8be5cf89c63dee9284a76b70f6eb1ce64
Francois Daoust <fd@tidoust.net>
2026-05-20 12:13:09 +0200

    `@webref/xref`: Support lookup of non percent-encoded URLs
    Via https://github.com/mdn/browser-compat-data/pull/23958#issuecomment-4495384695
    
    BCD does not use URL hashes that are percent-encoded. Webref does. It seems ok
    to support both forms in the lookup function.
    
    Note: this creates more cases where the `lookup()` may throw an `Error`. We may
    want to create a better-looking `ParameterError` at some point to make it clear
    to caller that these errors are due to a problem on their part. Keeping things
    simple for now.
    
    This update also fixes the code that indexes URLs from `alternateIds` to
    percent-encode the IDs, although note that the fix is hypothetical for the time
    being as there aren't any alternate ID that contains characters that need to be
    encoded.

--- test/xref/all.js
+++ test/xref/all.js
@@ -13,12 +13,12 @@ import { setup, lookup } from '@webref/xref';
 const scriptPath = path.dirname(fileURLToPath(import.meta.url));
 const rootFolder = path.join(scriptPath, '..', '..');
 
-describe('The @webref/xref code', function () {
+describe('The @webref/xref lookup() function', function () {
   before(function () {
     setup(rootFolder);
   });
 
-  it('can lookup a dfn', function () {
+  it('can find a dfn', function () {
     const url = 'https://html.spec.whatwg.org/multipage/webappapis.html#event-loop';
     const res = lookup(url);
     assert(res?.length, 'No dfn found');
@@ -28,7 +28,7 @@ describe('The @webref/xref code', function () {
     assert.strictEqual(entry.spec, 'html');
   });
 
-  it('can lookup a heading', function () {
+  it('can find a heading', function () {
     const url = 'https://compat.spec.whatwg.org/#css-at-rules';
     const res = lookup(url);
     assert(res?.length, 'No heading found');
@@ -38,7 +38,7 @@ describe('The @webref/xref code', function () {
     assert.strictEqual(entry.spec, 'compat');
   });
 
-  it('can lookup a dfn through a dev link', function () {
+  it('can find a dfn through a dev link', function () {
     const url = 'https://html.spec.whatwg.org/multipage/introduction.html#typographic-conventions%3Ax-that';
     const res = lookup(url);
     assert(res?.length, 'No dfn found');
@@ -48,7 +48,19 @@ describe('The @webref/xref code', function () {
     assert.strictEqual(entry.spec, 'html');
   });
 
-  it('can lookup a heading using an alternate ID', function () {
+  it('can find a dfn when the hash is not percent-encoded', function () {
+    const hash = 'typographic-conventions:x-that';
+    const baseUrl = 'https://html.spec.whatwg.org/multipage/introduction.html#';
+    const url = baseUrl + hash;
+    const res = lookup(url);
+    assert(res?.length, 'No dfn found');
+    assert.strictEqual(res[0].source, 'dfns');
+    const entry = res[0].entry;
+    assert.strictEqual(entry.links?.[0]?.href, baseUrl + encodeURIComponent(hash));
+    assert.strictEqual(entry.spec, 'html');
+  });
+
+  it('can find a heading using an alternate ID', function () {
     const fragment = 'contents';
     const url = `https://drafts.csswg.org/css-ui-4/#${fragment}`;
     const res = lookup(url);
@@ -59,7 +71,7 @@ describe('The @webref/xref code', function () {
     assert.strictEqual(entry.spec, 'css-ui-4');
   });
 
-  it('can lookup a dfn with a release URL', function () {
+  it('can find a dfn with a release URL', function () {
     const url = 'https://www.w3.org/TR/webcodecs/#videodecoder';
     const res = lookup(url);
     assert(res?.length, 'No dfn found');
@@ -73,5 +85,21 @@ describe('The @webref/xref code', function () {
     const url = 'https://compat.spec.whatwg.org/#idontexist';
     const res = lookup(url);
     assert.strictEqual(res?.length, 0);
+  });
+
+  it('throws when no URL is given', function () {
+    assert.throws(lookup, {
+      name: 'Error',
+      message: 'The `lookup()` function was called without argument.'
+    });
+  });
+
+  it('throws when the URL is invalid', function () {
+    assert.throws(function () {
+      lookup('invalid');
+    }, {
+      name: 'Error',
+      message: 'The `lookup()` function was called with an invalid URL.'
+    });
   });
 });
